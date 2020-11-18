1255f803fad02636dfb9c9394535c2e4f0f9fbfa
XYPB <duyx2000@gmail.com>
2020-11-19 01:38:59 +0800

    Finish script

---
+++ evalHandPred.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+# This script get the hand prediction result from different model
+
+GPU=0
+
+source ~/.bashrc
+cd utils
+if [[ "$(echo $?)" == 0 ]]; then
+    echo "python gen_json"
+    python gen_json.py
+fi
+
+cd ../hmr && conda activate hmr
+if [[ $? == 0 ]]; then
+    python -m demo
+fi
+
+cd ../hand_detector.d2 && conda activate 100doh
+if [[ $? == 0 ]]; then
+    CUDA_VISIBLE_DEVICES=$GPU python demo.py
+fi
+
